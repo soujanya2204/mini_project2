d1bdf685266c9fa621932445e527a8ccc13646ea
soujanya2204 <soujanyapandia@gmail.com>
2024-08-17 19:42:15 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,15 @@
+const input = document.getElementById("input");
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+function check() {
+  const value = input.value;
+  const reverse = reverseString(value);
+
+  if (value === reverse) {
+    alert("PALINDROME");
+  } else {
+    alert("NOT PALINDROME");
+  }
+  input.value = "";
+}
